1bef0bedbdee891052611a1e700893721420320b
Ivan Filipchik <wot.vania@mail.ru>
2020-09-23 21:38:12 +0300

    preloader for form

--- src/modules/senForm.js
+++ src/modules/senForm.js
@@ -3,7 +3,6 @@ const sendForm = () => {
 
    //Отправка формы
    const errorMessage = 'Что-то пошло не так...',
-      loadMessage = 'Загрузка',
       successMessage = 'Спасибо! Мы скоро с вами свяжемся';
    const allForm = document.querySelectorAll('form');
    allForm.forEach((form) => {
@@ -23,55 +22,66 @@ const sendForm = () => {
 
       form.addEventListener('submit', (e) => {
          e.preventDefault();
-         //    form.appendChild(statusMessage);
-         //    statusMessage.textContent = '';
+         form.appendChild(statusMessage);
+         statusMessage.textContent = '';
+         let animation = `<div class="preloader todelete">
+            <div class="preloader__row todelete">
+              <div class="preloader__item todelete"></div>
+              <div class="preloader__item todelete"></div>
+            </div>
+          </div>`;
 
+         form.insertAdjacentHTML('beforeend', animation);
 
-         //    const formData = new FormData(form);
-         //    let body = {};
-         //    formData.forEach((value, key) => {
-         //       body[key] = value;
-         //    });
-         //    const success = (request) => {
-         //       const todelete = Array.from(form.querySelectorAll('.todelete'));
-         //       todelete.forEach((item) => {
-         //          item.remove();
-         //       });
-         //       if (request.status !== 200) {
-         //          throw new Error('status network not 200');
-         //       }
-         //       statusMessage.textContent = successMessage;
-         //       const inputForm = form.querySelectorAll('input');
-         //       inputForm.forEach(elem => {
-         //          elem.value = '';
-         //       });
-         //       const deleteStatusMessage = () => {
-         //          statusMessage.remove();
-         //       };
-         //       setTimeout(deleteStatusMessage, 3000);
-         //    };
-         //    const error = () => {
-         //       statusMessage.textContent = errorMessage;
-         //       console.error();
-         //       const deleteStatusMessage = () => {
-         //          statusMessage.remove();
-         //       };
-         //       setTimeout(deleteStatusMessage, 3000);
-         //    };
-         //    postData(body)
-         //       .then(success)
-         //       .catch(error);
+         const formData = new FormData(form);
+         let body = {};
+         formData.forEach((value, key) => {
+            body[key] = value;
+         });
+         const success = (request) => {
+            const todelete = Array.from(form.querySelectorAll('.todelete'));
+            todelete.forEach((item) => {
+               item.remove();
+            });
+            if (request.status !== 200) {
+               throw new Error('status network not 200');
+            }
+            statusMessage.textContent = successMessage;
+            const inputForm = form.querySelectorAll('input');
+            inputForm.forEach(elem => {
+               elem.value = '';
+            });
+            const deleteStatusMessage = () => {
+               statusMessage.remove();
+            };
+            setTimeout(deleteStatusMessage, 3000);
+         };
+         const error = () => {
+            statusMessage.textContent = errorMessage;
+            console.error();
+            const deleteStatusMessage = () => {
+               statusMessage.remove();
+               const todelete = Array.from(form.querySelectorAll('.todelete'));
+               todelete.forEach((item) => {
+                  item.remove();
+               });
+            };
+            setTimeout(deleteStatusMessage, 3000);
+         };
+         postData(body)
+            .then(success)
+            .catch(error);
       });
 
-      // const postData = (body) => {
-      //    return fetch('./server.php', {
-      //       method: 'POST',
-      //       headers: {
-      //          'Content-Type': 'application/json'
-      //       },
-      //       body: JSON.stringify(body)
-      //    });
-      // };
+      const postData = (body) => {
+         return fetch('./server.php', {
+            method: 'POST',
+            headers: {
+               'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(body)
+         });
+      };
 
    });
 
